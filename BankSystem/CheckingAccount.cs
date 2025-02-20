d7bc79bd42504afddf702e1990e535e89e884504
LeotrimIslami <leotrimislami98@gmail.com>
2025-02-20 20:44:27 +0100

    Feat: Shtimi i klases trasheguese CheckingAccount nga klasa baze BankAccount

---
+++ BankSystem/CheckingAccount.cs
@@ -0,0 +1,6 @@
+﻿public class CheckingAccount : BankAccount    //Krijimi i klases CheckingAccount nga klasa baze BankAccount
+{
+
+    public CheckingAccount(string accountNumber, string ownerName, decimal initialBalance)
+        : base(accountNumber, ownerName, initialBalance) { }
+}
