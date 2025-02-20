aeeb24f1a455ad854a5c98b9677fde07abe51ecd
LeotrimIslami <leotrimislami98@gmail.com>
2025-02-20 21:07:02 +0100

    Feat: Shtimi i klases Transaction

---
+++ BankSystem/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Transaction
+{
+    public DateTime Date { get; }
+    public string Type { get; }
+    public decimal Amount { get; }
+
+    public Transaction(string type, decimal amount)
+    {
+        Date = DateTime.Now;
+        Type = type;
+        Amount = amount;
+    }
+
+    public override string ToString()
+    {
+        return $"{Date:G} | {Type} | {Amount:C}";
+    }
+}
