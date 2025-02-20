771666c45323283bd5735101912c57b305db536c
LeotrimIslami <leotrimislami98@gmail.com>
2025-02-20 21:25:24 +0100

    Feat: Shtimi i metodes per lejimin e vetem 3 terheqjeve mbrenda dites

--- BankSystem/CheckingAccount.cs
+++ BankSystem/CheckingAccount.cs
@@ -1,6 +1,26 @@
 ﻿public class CheckingAccount : BankAccount    //Krijimi i klases CheckingAccount nga klasa baze BankAccount
 {
+    private int dailyWithdrawals = 0;
+    private const int MaxWithdrawalsPerDay = 3;   //Shtimi i limitit per terheqje maksimale mbrenda dites
 
     public CheckingAccount(string accountNumber, string ownerName, decimal initialBalance)
         : base(accountNumber, ownerName, initialBalance) { }
+
+
+    public override bool Withdraw(decimal amount)
+    {
+        if (dailyWithdrawals < MaxWithdrawalsPerDay)
+        {
+            if (base.Withdraw(amount))
+            {
+                dailyWithdrawals++;
+                return true;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Daily withdrawal limit reached.");
+        }
+        return false;
+    }
 }
