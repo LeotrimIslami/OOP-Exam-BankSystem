0629acab5cd9866d5ddd630cb13dc752b3ee126a
LeotrimIslami <leotrimislami98@gmail.com>
2025-02-20 20:53:59 +0100

    Feat: Krijimi i struktures se programit dhe shtimi i Listes List<BankAccount>

--- BankSystem/Program.cs
+++ BankSystem/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<BankAccount> accounts = new List<BankAccount>();    //Shtimi i Listes se llogarive bankare
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\n1. Create Account\n2. Deposit\n3. Withdraw\n4. View Account\n5. Exit");
+            Console.Write("Choose an option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    CreateAccount();
+                    break;
+                case "2":
+                    MakeDeposit();
+                    break;
+                case "3":
+                    MakeWithdrawal();
+                    break;
+                case "4":
+                    ViewAccount();
+                    break;
+                case "5":
+                    Console.WriteLine("Exiting program...");
+                    Environment.Exit(0); // ✅ This closes the console
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+        }
+    }
+}
