23809fb9990505234693586a2c5d37e81c6c2619
LeotrimIslami <leotrimislami98@gmail.com>
2025-02-20 20:38:05 +0100

    Feat: Shtimi i klases trasheguese SavingsAccount nga klasa baze BankAccount

---
+++ BankSystem/SavingsAccount.cs
@@ -0,0 +1,14 @@
+﻿public class SavingsAccount : BankAccount    //Krijimi i klases SavingsAccount duke perdorur klasen baze BankAccount
+{
+    private const decimal InterestRate = 0.03m; // 3% interest
+
+    //Llogaria dhe shtimi i interesit ne balance
+    public SavingsAccount(string accountNumber, string ownerName, decimal initialBalance)
+        : base(accountNumber, ownerName, initialBalance) { }
+
+    public void CalculateInterest()
+    {
+        decimal interest = Balance * InterestRate;
+        Deposit(interest);
+        Console.WriteLine($"Interest of {interest:C} added.");
+    }
